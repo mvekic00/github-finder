d4547cf518c862e10c7b8e4f0d9ae68d49d94c0c
mvekic00 <mvekic00@fesb.hr>
2022-04-11 11:51:31 +0200

    using reducer

--- src/components/layout/Spinner.jsx
+++ src/components/layout/Spinner.jsx
@@ -4,7 +4,7 @@ import spinner from '../layout/assets/spinner.gif'
 function Spinner() {
   return (
     <div className='w-100 mt-20'>
-      <img width={180} className="text-center mx-auto" src={spinner} alt="Loading..."></img>
+      <img width={100} className="text-center mx-auto" src={spinner} alt="Loading..."></img>
     </div>
   )
 }
