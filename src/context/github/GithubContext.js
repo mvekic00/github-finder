1074658255e0f46167a26c0b4debf263446d830e
mvekic00 <mvekic00@fesb.hr>
2022-04-12 15:48:56 +0200

    alert_context&reducer

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -4,9 +4,11 @@ import githubReducer from "./GithubReducer";
 const GithubContext = createContext();
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+/* const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN; */
 
 export const GithubProvider = ({ children }) => {
+
+  
   const initialState = {
     users: [],
     loading: false,
@@ -39,11 +41,7 @@ export const GithubProvider = ({ children }) => {
       q:text
     })
     
-    const response = await fetch(`${GITHUB_URL}search/users?${params}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
+    const response = await fetch(`${GITHUB_URL}search/users?${params}`);
 
     const {items} = await response.json();
 
